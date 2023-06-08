e7dff6d34ac81890eb00b23da04abb6a48e5fbfc
Shintaro44 <shintaronemoto.6584@gmail.com>
2023-06-08 17:16:53 +0900

    商品出品機能追加の修正１回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.includes(:user).order('created_at DESC')
+    #@items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -21,37 +21,37 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
+  #def edit
     # ログインしているユーザーと同一であればeditファイルが読み込まれる
-    if @item.user_id == current_user.id && @item.order.nil?
-    else
-      redirect_to root_path
-    end
-  end
+  #  if @item.user_id == current_user.id && @item.order.nil?
+  #  else
+  #    redirect_to root_path
+  #  end
+  #end
 
-  def update
-    @item.update(item_params)
+  #def update
+  #  @item.update(item_params)
     # バリデーションがOKであれば詳細画面へ
-    if @item.valid?
-      redirect_to item_path(item_params)
-    else
+  #  if @item.valid?
+  #    redirect_to item_path(item_params)
+  #  else
       # NGであれば、エラー内容とデータを保持したままeditファイルを読み込み、エラーメッセージを表示させる
-      render 'edit'
-    end
-  end
+  #    render 'edit'
+  #  end
+  #end
 
-  def show
-  end
+  #def show
+  #end
 
-  def destroy
+  #def destroy
     # ログインしているユーザーと同一であればデータを削除する
-    if @item.user_id == current_user.id
-      @item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
-  end
+  #  if @item.user_id == current_user.id
+  #    @item.destroy
+  #    redirect_to root_path
+  #  else
+  #    redirect_to root_path
+  #  end
+  #end
 
   private
 
